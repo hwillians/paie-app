5d5ad5daa6bc96abf46b713d6008dc94630ef313
Helvin <hjwc86@gmail.com>
2020-09-13 00:04:14 +0200

    elements effacés

--- src/main/java/dev/paie/repositories/GradeReposirtory.java
+++
@@ -1,9 +0,0 @@
-package dev.paie.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.paie.entite.Grade;
-
-public interface GradeReposirtory extends JpaRepository<Grade, Integer> {
-
-}
--- src/main/java/dev/paie/repositories/ProfilRemuneratioRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.paie.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.paie.entite.ProfilRemuneration;
-
-public interface ProfilRemuneratioRepository extends JpaRepository<ProfilRemuneration, Integer> {
-
-}
